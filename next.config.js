522683e2d2fa622db35dcdce8a44086c03cb2185
camilo-mujica <camilo_mujica@hotmail.com>
2022-01-29 17:43:15 -0400

    pwa changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+
+const nextConfig = withPWA({
+    reactStrictMode: true,
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+    },
+});
+
+module.exports = nextConfig;
