7d7c3281157f24ed345e614a3d7d4a1fe3ce99bf
camilo-mujica <camilo_mujica@hotmail.com>
2022-01-29 17:40:53 -0400

    pwa changes

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    DocumentContext,
+} from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        return { ...initialProps };
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="manifest" href="/manifest/manifest.json" />
+                    <link rel="apple-touch-icon" href="/manifest/icon.png" />
+                    <meta name="theme-color" content="#fff" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
