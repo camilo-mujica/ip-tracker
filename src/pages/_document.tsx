312e008a5fb1edebebdd2504a4cb930c97f5341b
camilo-mujica <camilo_mujica@hotmail.com>
2022-01-29 17:56:50 -0400

    fixed pwa files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,8 +16,8 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel="manifest" href="/manifest/manifest.json" />
-                    <link rel="apple-touch-icon" href="/manifest/icon.png" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="apple-touch-icon" href="/icon.png" />
                     <meta name="theme-color" content="#fff" />
                 </Head>
                 <body>
